e5592c0b5c05d406682238ed011cd837aaabebfe
JanisJanis <janis.gailis@uia.no>
2017-01-30 14:08:05 +0100

    Some modifications to hello.go

--- hello.go
+++ hello.go
@@ -2,10 +2,14 @@ package main
 
 import "math"
 import "fmt"
+import "./fib"
 
 func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println(math.Log2(3))
 	fmt.Println(math.Log10(3))
-
+  	fn := fib.FibFunc()
+	fmt.Println(fn())
+	fmt.Println(fn())
+	fmt.Println(fn())
 }
